b3c91e979284f1f07f0b6e0be191193a8513fac2
Vineet Singh <vineet199927@gmail.com>
2023-07-29 19:24:34 +0530

    datatype conversion done

---
+++ 01_basics/01_variables/03_dataTypeConversionOperation.js
@@ -0,0 +1,38 @@
+let score = false
+
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+/** someDataType to Number
+ * "10" => 10
+ * "Hitesh" => NaN [Not A Number]
+ * true => 1; false => 0
+ */
+
+let someString = null
+let valueInBoolean = Boolean(someString)
+// console.log(typeof valueInBoolean)
+// console.log(valueInBoolean)
+
+/** someDataType to Boolean
+ * "Hitesh" => true
+ * "" => false
+ * undefined => false
+ * null => false
+ */
+
+let name = null
+let valueInString = String(name)
+console.log(valueInString);
+console.log(typeof valueInString);
+
+/** someDataType to string
+ * 1 => "1"
+ * true => "true", false => "false"
+ * undefined => "undefined"
+ * null => "null"
+ */
