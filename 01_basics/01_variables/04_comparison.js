14ee6187c9d8b2bdc21f0906fb0600f8dea4f091
Vineet Singh <vineet199927@gmail.com>
2023-07-30 11:35:34 +0530

    comparison,datatype,stack and heap done

---
+++ 01_basics/01_variables/04_comparison.js
@@ -0,0 +1,21 @@
+// Basic comparison operator
+
+// same datatype
+// console.log(5==5);
+// console.log(5>=5);
+// console.log(5<=5);
+// console.log(5!=5);
+
+// for different datatype
+// here converting string into number
+// console.log("1" == 1);
+// console.log("1" < 1);
+
+// checking for null values
+// console.log(null > 0);
+// // console.log(null == 0);
+// console.log(null <= 0);
+
+// console.log(undefined == 0);
+
+console.log("2" === 2);
---
+++ 01_basics/01_variables/06_stack_and_heap_memory.js
@@ -0,0 +1,16 @@
+// Stack (Primitive), Heap(Non-primitive)
+
+let myName = "Vineet"
+let myNewName = myName
+
+myNewName = "Aman"
+
+console.log(myName);
+console.log(myNewName);
+
+const hero = ["Captain", "IronMan","SpiderMan"]
+const newAgeHero = hero
+newAgeHero[0] = "Hanuman"
+
+console.log(newAgeHero);
+console.log(hero);
