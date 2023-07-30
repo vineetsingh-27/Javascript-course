93669d02e50b751e0424bde8dfec7adb8ae0bbc2
Vineet Singh <vineet199927@gmail.com>
2023-07-30 20:10:47 +0530

    Location changed

--- 01_basics/09_datesInJS.js
+++ 01_basics/09_datesInJS.js
@@ -2,7 +2,7 @@ let newDate = new Date()
 // console.log(newDate);
 
 // console.log(newDate.getMonth());
-let newDate1 = new Date('2023-08-15')
+let newDate1 = new Date('2023-08-27')
 console.log(newDate1.toLocaleString('default',{
     month : "long",
     weekday : "long"
