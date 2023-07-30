a0c6e3e5875a8174e61ba785122a41188277cda2
Vineet Singh <vineet199927@gmail.com>
2023-07-30 19:25:26 +0530

    Maths section done

---
+++ 01_basics/01_variables/08_nums_maths.js
@@ -0,0 +1,19 @@
+let num = 100000
+// console.log(num.toString())
+
+let num1 = 129.04
+// console.log(num1.toPrecision(4))
+
+let num2 = new Number(160)
+// console.log(num2) 
+
+// console.log(num.toLocaleString('en-IN'))
+
+// +++++++++++++++++++ Maths +++++++++++++++++++
+// console.log(Math.max(10,20,30)) 
+// console.log(Math.min(10,20,30)) 
+// console.log(Math.floor(2.56)) 
+// console.log(Math.ceil(2.06)) 
+// console.log(Math.pow(2,3)) 
+// console.log(Math.sqrt(25))
+console.log(Math.floor(Math.random()*10));
